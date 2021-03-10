ba5ab675886a1dffe01b95ebc539d2d7869a5a32
smetanskyy <stepansmetanskyy@gmail>
2021-03-10 06:49:34 +0200

    add changes

--- LibShare.Api/Data/Mapping/BookProfileMap.cs
+++ LibShare.Api/Data/Mapping/BookProfileMap.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using LibShare.Api.Data.ApiModels.ResponseApiModels;
 using LibShare.Api.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace LibShare.Api.Data.Mapping
 {
@@ -9,7 +12,34 @@ namespace LibShare.Api.Data.Mapping
         public BookProfileMap()
         {
             AllowNullCollections = true;
-            CreateMap<Book, BookApiModel>().ReverseMap();
+            CreateMap<Book, BookApiModel>()
+                .AfterMap<BookImageResolver>();
+
+            CreateMap<BookApiModel, Book>();
+        }
+    }
+
+    public class BookImageResolver : IMappingAction<Book, BookApiModel>
+    {
+        private readonly IHttpContextAccessor _accessor;
+        private readonly IConfiguration _configuration;
+
+        public BookImageResolver(IHttpContextAccessor accessor, IConfiguration configuration)
+        {
+            _accessor = accessor;
+            _configuration = configuration;
+        }
+
+        public void Process(Book source, BookApiModel destination, ResolutionContext context)
+        {
+            if (string.IsNullOrWhiteSpace(source.Image))
+                destination.Image = null;
+            else
+            {
+                var folderName = _configuration.GetValue<string>("UrlBooks");
+                var currentUrl = $"{_accessor.HttpContext.Request.Scheme}://{_accessor.HttpContext.Request.Host}";
+                destination.Image = Path.Combine(currentUrl, folderName, source.Image);
+            }
         }
     }
 }
